44d8a2c3220d69a6a730db663dc3a0ca55d6efc2
moath5532-lgtm <moath5532@gmail.com>
2026-06-17 01:14:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+from telegram.ext import ApplicationBuilder, CommandHandler
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'أهلاً {update.effective_user.first_name}، أنا بوت معاذ، أعمل الآن!')
+# هذا السطر يقرأ التوكن من الإعدادات التي حفظناها في موقع Render
+TOKEN = os.environ.get('TOKEN')
+
+async def hello(update, context):
+    await update.message.reply_text('أنا بوت الارنب، أعمل الآن!')
 
 if __name__ == '__main__':
-    TOKEN = '8926831653:AAHbduupIWl0cirNsdi5cGm6-IqPlm2xtK0'
     app = ApplicationBuilder().token(TOKEN).build()
-    
     app.add_handler(CommandHandler("start", hello))
-    
     print("البوت بدأ العمل...")
     app.run_polling()
